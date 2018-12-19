2f58ac454581dc199a6f16116bfc0f3242dcda29
ivy <original541029@gmail.com>
2018-12-19 14:52:42 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function () {
+  var plugins = [
+    autoprefixer({
+      browsers: ['last 3 version', '>5%']
+    }),
+  ];
   return gulp.src('./src/assets/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('./build/css'));
 });
 
