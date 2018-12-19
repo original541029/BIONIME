ed945e50f25a72d422b5545eb4ec9dd0a8a0663b
ivy <original541029@gmail.com>
2018-12-20 06:29:46 +0800

    add table and remove list row

--- build/js/site-list.js
+++ build/js/site-list.js
@@ -4,17 +4,68 @@ window.onload = function () {
   table();
 };
 
+var data = JSON.parse(localStorage.getItem("siteList")) || null;
+
 function table() {
-  var data = JSON.parse(localStorage.getItem("siteList")) || [];
+  $('.table').remove();
+  console.log(data);
+
+  if (data == null) {
+    return;
+  }
+
   var aryHead = ['站點', '修改時間', '動作'];
-  var dataLen = data.length;
   var objKeyData = Object.keys(data[0]);
+  var aryHeadLen = aryHead.length;
+  var dataLen = data.length;
   var objKeyDataLen = objKeyData.length;
+  var strThead = '<tr>';
+
+  for (var j = 0; j < aryHeadLen; j++) {
+    strThead += "<th>".concat(aryHead[j], "</th>");
+  }
+
+  strThead += '</tr>';
+  console.log(strThead);
+  var strTbody = '';
 
   for (var i = 0; i < dataLen; i++) {
-    for (var j = 0; j < objKeyDataLen; j++) {
-      console.log(data[i][objKeyData[j]]);
+    strTbody += "<tr>";
+
+    for (var _j = 0; _j < objKeyDataLen + 1; _j++) {
+      if (_j == 2) {
+        strTbody += "<td class=\"d-flex delete\"><div class=\"model-btn mr-2 cursor-potion\"><i class=\"fas fa-users\"></div></i><div class=\" cursor-potion text-danger\" data-num=\"".concat(i, "\">X</div></td>");
+      } else {
+        strTbody += "<td>".concat(data[i][objKeyData[_j]], "</td>");
+      }
     }
+
+    strTbody += '</tr>';
   }
+
+  console.log(strTbody);
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+  var table = document.createElement('table');
+  table.setAttribute('class', 'table');
+  var tableArea = document.querySelector('.table-area');
+  thead.innerHTML = strThead;
+  tbody.innerHTML = strTbody;
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  tableArea.appendChild(table);
+
+  if ($('.delete').length != 0) {
+    removeRow();
+  }
+}
+
+function removeRow() {
+  $('.delete').click(function (e) {
+    var num = e.target.dataset.num;
+    data.splice(num, 1);
+    localStorage.setItem("siteList", JSON.stringify(data));
+    table();
+  });
 }
 //# sourceMappingURL=site-list.js.map
--- src/assets/js/site-list.js
+++ src/assets/js/site-list.js
@@ -1,17 +1,63 @@
 window.onload = function () {
   table();
 }
+let data = JSON.parse(localStorage.getItem("siteList")) || null;
 
 function table() {
-  let data = JSON.parse(localStorage.getItem("siteList")) || [];
-  let aryHead = ['站點', '修改時間', '動作'];
-  let dataLen = data.length;
-  let objKeyData = Object.keys(data[0])
-  let objKeyDataLen = objKeyData.length;
-  for (let i = 0; i < dataLen; i++) {
-    for (let j = 0; j < objKeyDataLen; j++) {
-      console.log(data[i][objKeyData[j]])
-    }
-
+  $('.table').remove();
+  console.log(data)
+  if (data == null) {
+    return
   }
+  let aryHead = ['站點', '修改時間', '動作'];
+  let objKeyData = Object.keys(data[0])
+
+  let aryHeadLen = aryHead.length;
+  let dataLen = data.length;
+  let objKeyDataLen = objKeyData.length;
+  let strThead = '<tr>';
+
+
+  for (let j = 0; j < aryHeadLen; j++) {
+    strThead += `<th>${aryHead[j]}</th>`
+  }
+  strThead += '</tr>'
+  console.log(strThead)
+
+  let strTbody = '';
+  for (let i = 0; i < dataLen; i++) {
+    strTbody += `<tr>`;
+    for (let j = 0; j < (objKeyDataLen + 1); j++) {
+      if (j == 2) {
+        strTbody += `<td class="d-flex delete"><div class="model-btn mr-2 cursor-potion"><i class="fas fa-users"></div></i><div class=" cursor-potion text-danger" data-num="${i}">X</div></td>`
+      } else {
+        strTbody += `<td>${data[i][objKeyData[j]]}</td>`
+      }
+    }
+    strTbody += '</tr>'
+  }
+  console.log(strTbody)
+
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+  let table = document.createElement('table');
+  table.setAttribute('class', 'table')
+  let tableArea = document.querySelector('.table-area');
+  thead.innerHTML = strThead;
+  tbody.innerHTML = strTbody;
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  tableArea.appendChild(table);
+  if ($('.delete').length != 0) {
+    removeRow();
+  }
+}
+
+function removeRow() {
+  $('.delete').click((e) => {
+    var num = e.target.dataset.num;
+    data.splice(num, 1);
+    localStorage.setItem("siteList", JSON.stringify(data));
+    table();
+  })
 }
